cb1a26c9d027743f09092a4e6d8a9e940a490360
Ezra <ezraermy@gmail.com>
2023-08-10 14:23:26 +0300

    Create service page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
+import Solutions from "./components/Solutions";
 
 import { useEffect } from "react";
 // Animation package
@@ -23,6 +24,7 @@ const App = () => {
       <Home />
       <Skills />
       <Projects />
+      <Solutions />
       <footer className="p-3 text-center">
         <h6 className="mb-3">EZRA YEHUWALASHET</h6>
         <p>codeaprogram © All CopyRights Reserved 2022</p>
